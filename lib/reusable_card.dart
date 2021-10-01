9d6a96a404af0fe756c275e89d793cf25fad9f5d
kish-git <kishorraut5326@gmail.com>
2021-10-01 16:03:18 +0530

    Added GestureDetector & onPress()

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ReusableContainer extends StatelessWidget {
-  ReusableContainer({@required this.colour, this.cardChild});
+  ReusableContainer({@required this.colour, this.cardChild, this.onPress});
   final Color colour;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: colour, borderRadius: BorderRadius.circular(10.0)),
-      child: cardChild,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            color: colour, borderRadius: BorderRadius.circular(10.0)),
+        child: cardChild,
+      ),
     );
   }
 }
