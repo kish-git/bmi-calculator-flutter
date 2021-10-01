f252cb00300a5fe858c770be3d21c5d1d0b17211
kish-git <kishorraut5326@gmail.com>
2021-10-01 15:59:22 +0530

    Added colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
 
-const labelTextStyle = TextStyle(
+const kSliderActiveColour = Color(0xFFEB1555);
+const kSliderInactiveColour = Color(0xFF8D8E98);
+const kOverlayColour = Color(0x15EB1555);
+const kSliderActiveTrackColour = Colors.white;
+
+const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
 );
